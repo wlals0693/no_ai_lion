5c6b966d7055eb2e505846faf08e6fb3ab7c82a3
wlals0693 <jimin0693@gmail.com>
2026-07-18 10:00:15 +0900

    supabase CDN방식으로 변경

--- db.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient(
-  'https://aprhepkvvrywoqkktnry.supabase.co',
-  'sb_publishable_PfXlbOrRvNso2IIbmvf4HQ_ACGBxkfg',
-);
