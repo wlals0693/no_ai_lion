56add6b01f6fa912f1da749030119ff64533933a
wlals0693 <jimin0693@gmail.com>
2026-07-18 17:24:41 +0900

    crud기능 html추가

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ async function signUp(email, password) {
     email: email,
     password: password,
   });
-}
-
-async function signInWithEmail(email, password) {
-  const { data, error } = await supabaseclient.auth.signInWithPassword({
-    email: email,
-    password: password,
-  });
   return { data, error };
 }
 
@@ -18,10 +11,23 @@ if (signup_btn) {
   signup_btn.addEventListener('click', async () => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('pw').value;
-    await signUp(email, password);
-    alert('회원가입 성공');
-    window.location.href = 'login.html';
+    result = await signUp(email, password);
+    if (result.error) {
+      alert('회원가입 실패: ' + result.error.message);
+      return;
+    } else {
+      alert('회원가입 성공');
+      window.location.href = 'login.html';
+    }
   });
+}
+
+async function signInWithEmail(email, password) {
+  const { data, error } = await supabaseclient.auth.signInWithPassword({
+    email: email,
+    password: password,
+  });
+  return { data, error };
 }
 
 const login_btn = document.getElementById('login_btn');
@@ -37,4 +43,38 @@ if (login_btn) {
       window.location.href = 'index.html';
     }
   });
+}
+
+//crud 구현
+
+async function createpost(title, contents) {
+  const {
+    data: { user },
+  } = await supabaseclient.auth.getUser();
+
+  const { data, error } = await supabaseclient
+    .from('posts')
+    .insert({
+      title: title,
+      contents: contents,
+      userid: user.email,
+    })
+    .select();
+
+  return { data, error };
+}
+
+async function loadpost() {
+  const { data, error } = await supabaseclient.from('posts').select('*');
+  return { data, error };
+}
+
+async function edit_post(id, title, contents) {}
+
+async function delete_post(id) {
+  const { data, error } = await supabaseclient
+    .from('posts')
+    .delete()
+    .eq('id', id);
+  return { data, error };
 }
